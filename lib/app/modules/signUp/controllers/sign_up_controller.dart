36bae441c6a7062686626662480a20a2dfe903f3
Zobayer-Mahmud <zobayermahmud00@gmail.com>
2024-01-27 22:46:19 +0600

    sign in

--- lib/app/modules/signUp/controllers/sign_up_controller.dart
+++ lib/app/modules/signUp/controllers/sign_up_controller.dart
@@ -1,15 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:task_tracker/app/api/api_client/dio_client.dart';
 import 'package:task_tracker/app/api/data/response/model/auth/sign_in_response.dart';
 import 'package:task_tracker/app/api/data/response/model/auth/sign_up_model.dart';
 import 'package:task_tracker/app/api/service/auth_service/auth_service.dart';
 import 'package:task_tracker/app/common/app_constants.dart';
+import 'package:task_tracker/app/local_storage/local_storage.dart';
 import 'package:task_tracker/app/utils/utility.dart';
 
 class SignUpController extends GetxController {
   final AuthService authService = Get.find();
- late SharedPreferences sharedPreferences;
+  DioClient dioClient = Get.find();
+  LocalStorage localStorage = Get.find();
+  late SharedPreferences sharedPreferences;
+  GlobalKey<FormState> signUpKey = GlobalKey();
   @override
   void onInit() async {
     sharedPreferences = await SharedPreferences.getInstance();
@@ -21,12 +26,21 @@ class SignUpController extends GetxController {
   TextEditingController passwordController = TextEditingController();
   TextEditingController repeatPasswordController = TextEditingController();
   TextEditingController ageController = TextEditingController();
+
   signUp(BuildContext context) async {
-    SignUpModel signUpModel() => updateSignUpModel();
-    SignInResponse? signInResponse = await authService.signUp(signUpModel: signUpModel());
-    if(signInResponse!=null) {
-   if(signInResponse.token!=null)   sharedPreferences.setString(AppConstants.token, signInResponse.token!);
-      showSnackBar("Sign up successfully!", context);
+    if (passwordController.text != repeatPasswordController.text) {
+      showSnackBar("Password doesn't match", context);
+    } else if (signUpKey.currentState?.validate() == true) {
+      SignUpModel signUpModel() => updateSignUpModel();
+      SignInResponse? signInResponse =
+          await authService.signUp(signUpModel: signUpModel());
+      if (signInResponse != null) {
+        if (signInResponse.token != null) {
+          localStorage.saveData(AppConstants.token, signInResponse.token!);
+          dioClient.updateAuthToken(token: signInResponse.token);
+        }
+        showSnackBar("Sign up successfully!", context);
+      }
     }
   }
 
--- lib/app/modules/signUp/views/sign_up_view.dart
+++ lib/app/modules/signUp/views/sign_up_view.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:gap/gap.dart';
 import 'package:get/get.dart';
+import 'package:task_tracker/app/utils/extentions/extentions.dart';
 import 'package:task_tracker/gen/assets.gen.dart';
-
 import '../controllers/sign_up_controller.dart';
 
 class SignUpView extends GetView<SignUpController> {
@@ -48,75 +49,126 @@ class SignUpView extends GetView<SignUpController> {
         ),
         Padding(
           padding: const EdgeInsets.all(8.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              Text(
-                "Sign In",
-                style: Theme.of(context)
-                    .textTheme
-                    .headlineLarge
-                    ?.copyWith(color: const Color(0xff8C88CD)),
-              ),
-              const Gap(30),
-              Text("Name",
+          child: Form(
+            key: controller.signUpKey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Text(
+                  "Sign In",
                   style: Theme.of(context)
                       .textTheme
-                      .bodyMedium
-                      ?.copyWith(color: Colors.black)),
-              const Gap(10),
-              TextFormField(
-                controller: controller.nameController,
-              ),
-              const Gap(30),
-              Text("Email",
-                  style: Theme.of(context)
-                      .textTheme
-                      .bodyMedium
-                      ?.copyWith(color: Colors.black)),
-              const Gap(10),
-              TextFormField(
-                controller: controller.emailController,
-              ),
-              const Gap(30),
-              Text("Password",
-                  style: Theme.of(context)
-                      .textTheme
-                      .bodyMedium
-                      ?.copyWith(color: Colors.black)),
-              const Gap(10),
-              TextFormField(
-                controller: controller.passwordController,
-              ),
-              const Gap(30),
-              Text("Retype Password",
-                  style: Theme.of(context)
-                      .textTheme
-                      .bodyMedium
-                      ?.copyWith(color: Colors.black)),
-              const Gap(10),
-              TextFormField(
-                controller: controller.repeatPasswordController,
-              ),
-              const Gap(30),
-              Row(
-                children: [
-                  Expanded(
-                    child: ElevatedButton(
-                        onPressed: () async => await controller.signUp(context),
-                        child: Text(
-                          "Sign Up",
-                          style: Theme.of(context)
-                              .textTheme
-                              .bodyMedium
-                              ?.copyWith(color: Colors.white),
-                        )),
-                  ),
-                ],
-              ),
-              const Gap(10),
-            ],
+                      .headlineLarge
+                      ?.copyWith(color: const Color(0xff8C88CD)),
+                ),
+                const Gap(30),
+                Text("Name *",
+                    style: Theme.of(context)
+                        .textTheme
+                        .bodyMedium
+                        ?.copyWith(color: Colors.black)),
+                const Gap(10),
+                TextFormField(
+                  keyboardType: TextInputType.name,
+                  controller: controller.nameController,
+                  validator: (v) {
+                    if (v?.isEmpty == true) {
+                      return "Name is required";
+                    }
+                    return null;
+                  },
+                ),
+                const Gap(30),
+                Text("Email *",
+                    style: Theme.of(context)
+                        .textTheme
+                        .bodyMedium
+                        ?.copyWith(color: Colors.black)),
+                const Gap(10),
+                TextFormField(
+                  controller: controller.emailController,
+                  keyboardType: TextInputType.emailAddress,
+                  validator: (v) {
+                    if (v?.isNotEmpty == true) {
+                      if (v!.isValidEmail == true) {
+                        return null;
+                      } else {
+                        return "please, Enter a valid email";
+                      }
+                    } else {
+                      return "Email is required";
+                    }
+                  },
+                ),
+                const Gap(30),
+                Text("Age",
+                    style: Theme.of(context)
+                        .textTheme
+                        .bodyMedium
+                        ?.copyWith(color: Colors.black)),
+                const Gap(10),
+                TextFormField(
+                  controller: controller.ageController,
+                  inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+                  keyboardType: TextInputType.number,
+                ),
+                const Gap(30),
+                Text("Password *",
+                    style: Theme.of(context)
+                        .textTheme
+                        .bodyMedium
+                        ?.copyWith(color: Colors.black)),
+                const Gap(10),
+                TextFormField(
+                  controller: controller.passwordController,
+                  validator: (v) {
+                    if (v?.isEmpty == true) {
+                      return "Password is required";
+                    } else if (v!.length < 7) {
+                      return "Password must contain at least 7 character";
+                    }
+                    return null;
+                  },
+                ),
+                const Gap(30),
+                Text("Retype Password",
+                    style: Theme.of(context)
+                        .textTheme
+                        .bodyMedium
+                        ?.copyWith(color: Colors.black)),
+                const Gap(10),
+                TextFormField(
+                  controller: controller.repeatPasswordController,
+                  validator: (v) {
+                    if (v?.isEmpty == true) {
+                      return "Password is required";
+                    } else if (v!.length < 7) {
+                      return "Password must contain at least 7 character";
+                    }
+                    return null;
+                  },
+                ),
+                const Gap(30),
+                Row(
+                  children: [
+                    Expanded(
+                      child: ElevatedButton(
+                          onPressed: () async =>
+                              await controller.signUp(context),
+                          child: Text(
+                            "Sign Up",
+                            style: Theme.of(context)
+                                .textTheme
+                                .bodyMedium
+                                ?.copyWith(color: Colors.white),
+                          )),
+                    ),
+                  ],
+                ),
+                const Gap(10),
+              ],
+            ),
           ),
         ),
       ],
